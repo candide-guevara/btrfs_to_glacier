b4cbe1142cd3ddb4a31b6a3d73e6f3e08cb09996
candide guevara <candide-guevara@users.noreply.github.com>
2023-09-21 20:51:28 +0200

    introduce sync closer for compressed sink

--- src/golang/encryption/compressed_io.go
+++ src/golang/encryption/compressed_io.go
@@ -2,6 +2,7 @@ package encryption
 
 import (
   "compress/gzip"
+  "context"
   "io"
 
   "btrfs_to_glacier/util"
@@ -14,27 +15,51 @@ func NewCompressingSource_Gzip(source io.Reader) io.ReadCloser {
   reader, writer := io.Pipe()
   gzip_w := gzip.NewWriter(writer)
   go func() {
+    var fls_err, cmp_err error
     count, cpy_err := io.Copy(gzip_w, source)
-    // BE CAREFUL IT IS A TRAP !
-    // `gzip.Writer.Close()` does not flush.
-    fls_err := gzip_w.Flush()
-    cmp_err := gzip_w.Close()
+    // Do not write anything else in case there was a copy error
+    if cpy_err == nil {
+      // BE CAREFUL IT IS A TRAP !
+      // `gzip.Writer.Close()` does not flush.
+      fls_err = gzip_w.Flush()
+      cmp_err = gzip_w.Close()
+    }
     pip_err := writer.CloseWithError(util.Coalesce(cpy_err, fls_err, cmp_err))
     all_err := util.Coalesce(cpy_err, fls_err, cmp_err, pip_err)
-    if all_err != nil { util.Warnf("NewCompressingSource: count=%d, %v", count, all_err) }
+    if all_err != nil { util.Warnf("NewCompressingSource_Gzip: count=%d, %v", count, all_err) }
   }()
   return reader
 }
 
 func NewCompressingSource(source io.Reader) io.ReadCloser {
   return io.NopCloser(source)
+  //return NewCompressingSource_Gzip(source)
+}
+
+type syncCloser struct {
+  io.WriteCloser
+  ctx context.Context
+  done chan bool
+}
+
+func(self syncCloser) Close() error {
+  err := self.WriteCloser.Close()
+  //util.Warnf("syncCloser.Close: %v", err)
+  if err != nil { return err }
+  select {
+    case <-self.done:
+    case <-self.ctx.Done(): return self.ctx.Err()
+  }
+  return nil
 }
 
 // Compressed data must be written to the returned `writer`.
 // `sink` will be written with the uncompressed data.
 // `sink` will not be closed by this method.
+// IMPORTANT: the returned writer will block until all data has gone to `sink` when closing.
 // Does not return errors upon creation on purpose. It is easier for error handling in callign code.
-func NewDecompressingSink_Gzip(sink io.Writer) io.WriteCloser {
+func NewDecompressingSink_Gzip(ctx context.Context, sink io.Writer) io.WriteCloser {
+  done := make(chan bool)
   reader, writer := io.Pipe()
 
   go func() {
@@ -49,14 +74,18 @@ func NewDecompressingSink_Gzip(sink io.Writer) io.WriteCloser {
     }
     pip_err := reader.CloseWithError(util.Coalesce(new_err, cpy_err, cmp_err))
     all_err := util.Coalesce(new_err, cpy_err, cmp_err, pip_err)
-    if all_err != nil { util.Warnf("NewDecompressingSink: count=%d, %v", count, all_err) }
+    if all_err != nil { util.Warnf("NewDecompressingSink_Gzip: count=%d, %v", count, all_err) }
+    util.Warnf("first")
+    close(done)
   }()
-  return writer
+  return syncCloser{writer, ctx, done}
 }
 
 type nopCloser struct { io.Writer }
 func (self nopCloser) Close() error { return nil }
-func NewDecompressingSink(sink io.Writer) io.WriteCloser {
+
+func NewDecompressingSink(ctx context.Context, sink io.Writer) io.WriteCloser {
   return nopCloser{sink}
+  //return NewDecompressingSink_Gzip(ctx, sink)
 }
 
--- src/golang/encryption/compressed_io_test.go
+++ src/golang/encryption/compressed_io_test.go
@@ -8,13 +8,16 @@ import (
 
   "btrfs_to_glacier/types/mocks"
   "btrfs_to_glacier/util"
+  "btrfs_to_glacier/types"
 )
 
 func TestGzipSink_BadData(t *testing.T) {
+  ctx,cancel := context.WithTimeout(context.Background(), util.TestTimeout)
+  defer cancel()
   buf := new(bytes.Buffer)
   expect_msg := []byte("this is some plain text data")
 
-  gzip_w := NewDecompressingSink_Gzip(buf)
+  gzip_w := NewDecompressingSink_Gzip(ctx, buf)
   // The first write races with the initialization of the gzip reader,
   // so the errors has not time to propagate.
   gzip_w.Write(expect_msg)
@@ -26,12 +29,14 @@ func TestGzipSink_BadData(t *testing.T) {
 }
 
 func TestGzipSmallMsg(t *testing.T) {
+  ctx,cancel := context.WithTimeout(context.Background(), util.TestTimeout)
+  defer cancel()
   buf := new(bytes.Buffer)
   expect_msg := []byte("this is some plain text data")
   read_pipe := mocks.NewPreloadedPipe(expect_msg).ReadEnd()
 
   gzip_r := NewCompressingSource_Gzip(read_pipe)
-  gzip_w := NewDecompressingSink_Gzip(buf)
+  gzip_w := NewDecompressingSink_Gzip(ctx, buf)
   count, err := io.Copy(gzip_w, gzip_r)
   if err != nil { t.Errorf("io.Copy: count=%d, %v", count, err) }
   err = gzip_w.Close()
@@ -62,7 +67,7 @@ func TestGzipSmallMsg_ManyWrites(t *testing.T) {
   }()
 
   gzip_r := NewCompressingSource_Gzip(reader)
-  gzip_w := NewDecompressingSink_Gzip(buf)
+  gzip_w := NewDecompressingSink_Gzip(ctx, buf)
   count, err := io.Copy(gzip_w, gzip_r)
   if err != nil { t.Errorf("io.Copy: count=%d, %v", count, err) }
   err = gzip_w.Close()
@@ -83,7 +88,7 @@ func TestGzipLargeMsg(t *testing.T) {
   read_pipe := mocks.NewBigPreloadedPipe(ctx, expect_msg).ReadEnd()
 
   gzip_r := NewCompressingSource_Gzip(read_pipe)
-  gzip_w := NewDecompressingSink_Gzip(buf)
+  gzip_w := NewDecompressingSink_Gzip(ctx, buf)
   count, err := io.Copy(gzip_w, gzip_r)
   if err != nil { t.Errorf("io.Copy: count=%d, %v", count, err) }
   err = gzip_w.Close()
@@ -94,18 +99,15 @@ func TestGzipLargeMsg(t *testing.T) {
   }
 }
 
-func TestGzipLargeMsg_ViaPipe(t *testing.T) {
-  ctx,cancel := context.WithTimeout(context.Background(), util.TestTimeout)
-  defer cancel()
-
-  pipe_r, pipe_w := io.Pipe()
+func testGzipLargeMsg_ViaPipe_Helper(
+    ctx context.Context, t *testing.T, pipe_r io.ReadCloser, pipe_w io.WriteCloser) {
   buf := new(bytes.Buffer)
   alphabet := []byte("repeat this short message forever and ever.")
   expect_msg := bytes.Repeat(alphabet, 4096)
   read_pipe := mocks.NewBigPreloadedPipe(ctx, expect_msg).ReadEnd()
 
   gzip_r := NewCompressingSource_Gzip(read_pipe)
-  gzip_w := NewDecompressingSink_Gzip(buf)
+  gzip_w := NewDecompressingSink_Gzip(ctx, buf)
   done_r := make(chan error)
   done_w := make(chan error)
 
@@ -127,5 +129,59 @@ func TestGzipLargeMsg_ViaPipe(t *testing.T) {
   util.WaitForClosure(t, ctx, done_r)
   util.WaitForClosure(t, ctx, done_w)
   util.EqualsOrFailTest(t, "Bad length", buf.Len(), len(expect_msg))
+}
+
+func TestGzipLargeMsg_ViaPipe(t *testing.T) {
+  ctx,cancel := context.WithTimeout(context.Background(), util.TestTimeout)
+  defer cancel()
+
+  pipes := []types.Pipe{
+    util.NewFileBasedPipe(ctx),
+    util.NewInMemPipe(ctx),
+  }
+  for _,pipe := range pipes {
+    testGzipLargeMsg_ViaPipe_Helper(ctx, t, pipe.ReadEnd(), pipe.WriteEnd())
+  }
+}
+
+func testGzipSinkCloseRace_Helper(
+    ctx context.Context, t *testing.T, pipe_r io.ReadCloser, pipe_w io.WriteCloser) {
+  buf := new(bytes.Buffer)
+  expect_msg := []byte("this is some plain text data")
+  read_pipe := mocks.NewPreloadedPipe(expect_msg).ReadEnd()
+
+  done := make(chan error)
+  go func() {
+    count, err := io.Copy(buf, pipe_r)
+    if err != nil { t.Errorf("io.Copy: count=%d, %v", count, err) }
+    close(done)
+  }()
+
+  gzip_r := NewCompressingSource_Gzip(read_pipe)
+  gzip_w := NewDecompressingSink_Gzip(ctx, pipe_w)
+  count, err := io.Copy(gzip_w, gzip_r)
+  if err != nil { t.Errorf("io.Copy: count=%d, %v", count, err) }
+
+  err = gzip_w.Close()
+  if err != nil { t.Errorf("gzip_w.Close: count=%d, %v", count, err) }
+  util.Warnf("second")
+  err = pipe_w.Close()
+  if err != nil { t.Errorf("pipe_w.Close: count=%d, %v", count, err) }
+
+  util.WaitForClosure(t, ctx, done)
+  util.EqualsOrFailTest(t, "Bad length", buf.Len(), len(expect_msg))
+}
+
+func TestGzipSinkCloseRace(t *testing.T) {
+  ctx,cancel := context.WithTimeout(context.Background(), util.TestTimeout)
+  defer cancel()
+
+  pipes := []types.Pipe{
+    util.NewFileBasedPipe(ctx),
+    util.NewInMemPipe(ctx),
+  }
+  for _,pipe := range pipes {
+    testGzipSinkCloseRace_Helper(ctx, t, pipe.ReadEnd(), pipe.WriteEnd())
+  }
 }
 
