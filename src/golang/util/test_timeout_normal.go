362950c8fd5106743a247d35f1fc929737dd239f
candide guevara <candide-guevara@users.noreply.github.com>
2023-09-24 12:09:59 +0200

    increase test timeout to account for extra cpu cycles in compression

--- src/golang/util/test_timeout_normal.go
+++ src/golang/util/test_timeout_normal.go
@@ -6,11 +6,11 @@ package util
 import "time"
 
 // https://stackoverflow.com/questions/44944959/how-can-i-check-if-the-race-detector-is-enabled-at-runtime
-const TestTimeout  = 10 *  time.Millisecond
+const TestTimeout  = 12 *  time.Millisecond
 const SmallTimeout = 2 *   time.Millisecond
 const MedTimeout   = 5 *   time.Millisecond
-const LargeTimeout = 17 *  time.Millisecond
-const HugeTimeout =  170 * time.Millisecond
+const LargeTimeout = 19 *  time.Millisecond
+const HugeTimeout =  190 * time.Millisecond
 const RaceDetectorOn = false
 
 
