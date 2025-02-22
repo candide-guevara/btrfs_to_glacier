6dbbfa25e50d9c909d3641be0c959b4688652ce4
candide guevara <candide-guevara@users.noreply.github.com>
2025-02-22 17:45:42 +0100

    tweak to example on managing iam users

--- etc/aws_iam_config/aws_iam_commands_example.sh
+++ etc/aws_iam_config/aws_iam_commands_example.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 aws iam list-users
-aws iam list-attached-user-policies --user-name=btrfs_experimental
-aws iam get-policy --policy-arn=arn:aws:iam::096187466395:policy/btrfs_experimetal_dyn_policy
+aws iam list-attached-user-policies \
+  --user-name=btrfs_experimental
+aws iam get-policy \
+  --policy-arn=arn:aws:iam::096187466395:policy/btrfs_experimental_s3_policy
+aws iam get-policy-version \
+  --policy-arn=arn:aws:iam::096187466395:policy/btrfs_experimental_s3_policy \
+  --version-id=v1
 
